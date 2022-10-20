6a3fa81a7b75b86ff42f37bed293bdb3fcce9482
Lucas Cartofield <frgt_10@outlook.com>
2022-10-20 00:02:01 -0300

    Agregar archivos de proyecto.

---
+++ Programa_Promedio/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Programa_Promedio
+{
+    partial class Promedio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.txtNota4 = new System.Windows.Forms.TextBox();
+            this.btnPromedio = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NOTA 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NOTA 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "NOTA 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "NOTA 4";
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Location = new System.Drawing.Point(123, 23);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(100, 22);
+            this.txtNota1.TabIndex = 4;
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Location = new System.Drawing.Point(123, 73);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(100, 22);
+            this.txtNota2.TabIndex = 5;
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.Location = new System.Drawing.Point(123, 126);
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(100, 22);
+            this.txtNota3.TabIndex = 6;
+            // 
+            // txtNota4
+            // 
+            this.txtNota4.Location = new System.Drawing.Point(123, 179);
+            this.txtNota4.Name = "txtNota4";
+            this.txtNota4.Size = new System.Drawing.Size(100, 22);
+            this.txtNota4.TabIndex = 7;
+            // 
+            // btnPromedio
+            // 
+            this.btnPromedio.Location = new System.Drawing.Point(31, 236);
+            this.btnPromedio.Name = "btnPromedio";
+            this.btnPromedio.Size = new System.Drawing.Size(192, 37);
+            this.btnPromedio.TabIndex = 8;
+            this.btnPromedio.Text = "PROMEDIO";
+            this.btnPromedio.UseVisualStyleBackColor = true;
+            this.btnPromedio.Click += new System.EventHandler(this.btnPromedio_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(31, 295);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(192, 37);
+            this.btnLimpiar.TabIndex = 9;
+            this.btnLimpiar.Text = "LIMPIAR";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // Promedio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 355);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnPromedio);
+            this.Controls.Add(this.txtNota4);
+            this.Controls.Add(this.txtNota3);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.txtNota1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Promedio";
+            this.Text = "Promedio";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.TextBox txtNota4;
+        private System.Windows.Forms.Button btnPromedio;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ Programa_Promedio/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programa_Promedio
+{
+    public partial class Promedio : Form
+    {
+        public Promedio()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPromedio_Click(object sender, EventArgs e)
+        {
+            decimal nota1 = Convert.ToDecimal(txtNota1.Text);
+            decimal nota2 = Convert.ToDecimal(txtNota2.Text);
+            decimal nota3 = Convert.ToDecimal(txtNota3.Text);
+            decimal nota4 = Convert.ToDecimal(txtNota4.Text);
+
+            //busca la mayor nota
+
+            decimal[] notas = { nota1, nota2, nota3, nota4 };
+            decimal mayor = notas.Max();
+
+            //calculo promedio
+
+            decimal promedio = (nota1 + nota2 + nota3 + nota4) / 4;
+
+            if (promedio >= 9)
+            {
+                MessageBox.Show("Mayor nota: " + mayor + " Promedio: " + promedio + " Excelente");
+            }
+            else if (promedio >= 7 & promedio < 9)
+            {
+                MessageBox.Show("Mayor nota: " + mayor + " Promedio: " + promedio + " Muy Bien");
+            }
+            else if (promedio < 7 & promedio >= 4)
+            {
+                MessageBox.Show("Mayor nota: " + mayor + " Promedio: " + promedio + " Regular");
+            }
+            else
+            {
+                MessageBox.Show("Mayor nota: " + mayor + " Promedio: " + promedio + " En proceso de aprendizaje");
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtNota1.Clear();
+            txtNota2.Clear();
+            txtNota3.Clear();
+            txtNota4.Clear();
+        }
+    }
+}
